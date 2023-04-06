2d7860d30e2033c001f9ea15d92b896d4d93b67c
kenethtech <77956449+kenethtech@users.noreply.github.com>
2023-04-06 11:25:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+  function title(){
+  	
+  	echo "Luq's Cleaning Services Limited";
+
+  }
+
+ ?>
+<!-- Header here --->
+ <?php require_once('includes/header.php')?>
+
+<!--- Navigation bar ---->
+<?php require_once('includes/navbar.php')?>
+
+
+ <?php require_once('includes/footer.php')?>
